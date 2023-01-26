64e4bcd85cdf6687afac42bdb129fa4243b7016b
VetheonGames <ceo@pixelatedstudios.net>
2023-01-25 17:17:18 -0700

    Add some methods for validating the config before we pass it's contents to the server

--- configmanager.rb
+++ configmanager.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 class Config
-  attr_reader :config
+  attr_reader :config, :api_key, :server_ip, :server_port
 
   def initialize
     @config = JSON.parse(File.read('config.json'))
-  end
-
-  def server_ip
-    config['server']['ip']
-  end
-
-  def server_port
-    config['server']['port']
+    @server_ip = @config['server']['ip']
+    @server_port = @config['server']['port']
+    @api_key = @config['api_key']
+  rescue Errno::ENOENT => e
+    puts "Error: config.json file not found, please check that the file exists in the current directory: #{e.class}"
+    exit
+  rescue JSON::ParserError => e
+    puts "Error: config.json file is not valid JSON, please check the format of the file: #{e.class}"
+    exit
   end
 end
