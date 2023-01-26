21e00d572437a8ea8673116851f9c81d4fb98040
VetheonGames <ceo@pixelatedstudios.net>
2023-01-25 17:17:41 -0700

    create a checker to see if the config exists, and create one with default settings if it doesn't

---
+++ configchecker.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ConfigChecker
+  def self.create_config_file
+    File.open("config.json", "w") do |f|
+      f.write(default_config)
+    end
+  end
+
+  def self.default_config
+    {
+      "server": {
+        "ip": "0.0.0.0",
+        "port": "4567"
+      },
+      "api_key":"API_KEY"
+    }.to_json
+  end
+end
+
